15fa0307feedb5d856797251729dfccf3e16d564
Arnar Gauti Ingason <granra@gmail.com>
2015-08-16 22:54:38 +0000

    Fixing wifi.sh

--- panel/scripts/wifi.sh
+++ panel/scripts/wifi.sh
@@ -3,14 +3,15 @@
 dir=$(dirname $0)
 source $dir/config
 
+interface=wlp3s0
+
 wifi() {
 	WIFI_SSID=$(iwconfig $interface | awk 'NR==1 {print $4}' | sed -r 's/ESSID:"(.+)"/\1/');
-	STRENGTH=$(iwconfig $interface | awk 'NR==6 {print $4}' | sed -r 's/level=([0-9]+)\/100/\1/');
-	if [ -z $WIFI_SSID ]
+	if [ $WIFI_SSID == "ESSID:off/any" ]
 	then
 		echo 'P'"%{F$COLOR_RED}"$WIFI "Down%{F-}"
 	else
-		echo 'P'$WIFI $STRENGTH% - $WIFI_SSID
+		echo 'P'$WIFI $WIFI_SSID
 	fi
 }
 
